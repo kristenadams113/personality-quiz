6639ba61b5140bc3b111d9d7aa33c47600229e92
Glitch (personality-quiz-starter-code) <none>
2020-08-13 14:16:36 +0000

    💈💰 Checkpoint ./script.js:20595765/1130

--- script.js
+++ script.js
@@ -16,19 +16,25 @@ var q1a2 = document.getElementById("q1a2");
 var q1a3 = document.getElementById("q1a3");
 var q1a4 = document.getElementById("q1a4");
 
-//#TODO: Use the DOM to create variables for the first quiz question.
+//# variables for second quiz question
 var q2a1 = document.getElementById("q2a1");
 var q2a2 = document.getElementById("q2a2");
 var q2a3 = document.getElementById("q2a3");
 var q2a4 = document.getElementById("q2a4");
+
+//# variables for third quiz question
 var q3a1 = document.getElementById("q3a1");
 var q3a2 = document.getElementById("q3a2");
 var q3a3 = document.getElementById("q3a3");
 var q3a4 = document.getElementById("q3a4");
+
+//# variables for fourth quiz question
 var q4a1 = document.getElementById("q4a1");
 var q4a2 = document.getElementById("q4a2");
 var q4a3 = document.getElementById("q4a3");
 var q4a4 = document.getElementById("q4a4");
+
+//# variables for fifth quiz question
 var q5a1 = document.getElementById("q5a1");
 var q5a2 = document.getElementById("q5a2");
 var q5a3 = document.getElementById("q5a3");
@@ -46,12 +52,26 @@ var q5a4 = document.getElementById("q5a4");
 
 
 //#TODO: Add Event Listeners to your answer choice variables.
-q1a1.addEventListener("click", result1);
-q1a2.addEventListener("click",result3);
-q1a3.addEventListener("click", result2);
-q1a4.addEventListener("click", result4);
-q2a1.addEventListener("click", result3);
-q2a2.addEventListener("click", result3);
+q1a1.addEventListener("click", heather);
+q1a2.addEventListener("click", checkmate);
+q1a3.addEventListener("click", grow);
+q1a4.addEventListener("click", lookalike);
+q2a1.addEventListener("click", checkmate);
+q2a2.addEventListener("click", heather);
+q2a3.addEventListener("click", lookalike);
+q2a4.addEventListener("click", grow);
+q3a1.addEventListener("click", lookalike);
+q3a2.addEventListener("click", checkmate);
+q3a3.addEventListener("click", grow);
+q3a4.addEventListener("click", heather);
+q4a1.addEventListener("click", checkmate);
+q4a2.addEventListener("click", heather);
+q4a3.addEventListener("click", lookalike);
+q4a4.addEventListener("click", grow);
+q5a1.addEventListener("click", lookalike);
+q5a2.addEventListener("click", grow);
+q5a3.addEventListener("click", heather);
+q5a4.addEventListener("click", checkmate);
 
 
 
@@ -60,8 +80,19 @@ q2a2.addEventListener("click", result3);
 
 
 //#TODO: Define quiz functions here
-function result1(){
+
+function heather(){
+
+}
+
+function checkmate(){
   
+}
+
+function grow(){
   
+}
+
+function lookalike(){
   
 }
